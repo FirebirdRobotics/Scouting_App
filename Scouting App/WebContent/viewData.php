5fa6198784f79409b166d98dc574a3aa37fa2f09
ricefarmchild <33813750+ricefarmchild@users.noreply.github.com>
2018-02-01 16:52:08 -0700

    Update viewData.php

--- Scouting App/WebContent/viewData.php
+++ Scouting App/WebContent/viewData.php
@@ -77,39 +77,13 @@ $(document).ready(function() {
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
-
-	//echo "<pre>";
-	//print_r($_POST);
-	//echo "</pre>";	
-	
-	// Add variables
-	$robot_number = mysqli_real_escape_string($conn, $_POST['robotNumber']);
-	$crossed_baseline = mysqli_real_escape_string($conn, $_POST['crossedBaseline']); // for these four sets, we need to have it so that when the data is taken from the database, it displays something else
-	$place_cube_auto = mysqli_real_escape_string($conn, $_POST['placedCubeAuto']); // above comment
-	$ally_switch = mysqli_real_escape_string($conn, $_POST['allySwitch']);
-	$enemy_switch = mysqli_real_escape_string($conn, $_POST['enemySwitch']);
-	$scale_cube = mysqli_real_escape_string($conn, $_POST['scale']);
-	$attempt_climb = mysqli_real_escape_string($conn, $_POST['attemptedClimb']); // above comment
-	$carry_robots = mysqli_real_escape_string($conn, $_POST['carriedRobots']); // above comment
-	$comments = mysqli_real_escape_string($conn, $_POST['comments']);
-	
-	// Insert the above variables into the table values
-	$sql="REPLACE INTO robots (`robotNumber`, `crossedBaseline`, `placedCubeAuto`, `allySwitch`, `enemySwitch`, `scale`, `attemptedClimb`, `carriedRobots`, `comments`)
-		               VALUES ('$robot_number', '$crossed_baseline', '$place_cube_auto', '$ally_switch', '$enemy_switch', '$scale_cube', '$attempt_climb', '$carry_robots', '$comments')";
-	
-	
-	if ($conn->query($sql) === TRUE) {
-	    echo "New record created successfully";
-	} else {
-	    echo "Error: " . $sql . "<br>" . $conn->error;
-	}
 	
 	echo "<br><br>";
 	?>
 	<table class="display" id="table1"><thead>
 	<?php 
 	echo "<tr>
-                    <th>Robot Numberr</th>
+                    <th>Robot Number</th>
                     <th>Crossed Baseline?</th>
                     <th>Placed Cube Autonomous</th>
                     <th>Cubes on Ally Switch</th>
