dbdc8d10bd590d4c36d0de2216849c02811f973d
ricefarmchild <33813750+ricefarmchild@users.noreply.github.com>
2018-03-09 10:51:46 -0700

    Update addNewRobot.php

--- Scouting App/WebContent/addNewRobot.php
+++ Scouting App/WebContent/addNewRobot.php
@@ -43,28 +43,9 @@
     		
 		?>
 		
-		<select name='matchNumber' class='dropdown-button'>
-			<option value='' disabled selected>Round #</option>
-			<option value='1'>1</option>
-			<option value='2'>2</option>
-			<option value='3'>3</option>
-			<option value='4'>4</option>
-			<option value='5'>5</option>
-			<option value='6'>6</option>
-			<option value='7'>7</option>
-			<option value='8'>8</option>
-			<option value='9'>9</option>
-			<option value='10'>10</option>
-			<option value='11'>11</option>
-			<option value='12'>12</option>
-			<option value='13'>13</option>
-			<option value='14'>14</option>
-			<option value='15'>15</option>
-			<option value='16'>16</option>
-		</select>
-		
-		<br><br>
-		
+		<input type='text' name='matchNumber' style='max-width:100px' placeholder='Match #'>
+			
+		<br><br> 
 		
 		
 		<b><font size="+3">Autonomous:</font></b><br>
