b5d39d82792a70b6613f96d0c0e705b5388d281b
ricefarmchild <33813750+ricefarmchild@users.noreply.github.com>
2018-03-29 14:57:46 -0700

    Add files via upload

--- Scouting App/WebContent/deleteRecordsAction.php
+++ Scouting App/WebContent/deleteRecordsAction.php
@@ -9,10 +9,10 @@
 	include("database.php");   
 	
 	$delete_option = mysqli_real_escape_string($conn, $_POST['deleteRecords']);
-	$admin_code = mysqli_real_escape_string($conn, $_POST['adminCode'])
+	$admin_code = mysqli_real_escape_string($conn, $_POST['adminCode']);
 	
-	if () {
-	    die()
+	if ($admin_code != 'Admin3019') {
+	    die('Invalid code' . '<br><a href="home.php">Click here to return to Home</a>');
 	}
 	
 	// STILL NEED TO TEST
@@ -20,6 +20,8 @@
 	    $sql = "TRUNCATE TABLE 'teams'";
 	} else if ($delete_option == 'deleteRecords-robots') {
 	    $sql = "TRUNCATE TABLE 'robots'";
+	} else {
+	    die('No option selected' . '<br><a href="deleteRecords.php">Click here to go back</a>');
 	}
 	
 	if ($conn->query($sql) === TRUE) {
